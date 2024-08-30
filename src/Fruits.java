646bf274e264e66de96058c3d1bd3df3e30259ee
raiani <raiani.guimaraes@zup.com.br>
2024-08-30 16:05:54 -0300

    adding fruits class

--- src/Fruits.java
+++ src/Fruits.java
@@ -1,24 +1,27 @@
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public enum Fruits {
 
-    UVA("Uva", 30),
-    MORANGO("Morango", 20),
-    BANANA("Banana", 10);
+     UVA("Uva", 30, "nivel 1"),
+     MORANGO("Morango", 20, "nivel 1"),
+     BANANA("Banana", 10, "nivel 1"),
+    JABOTICABACA ("Jaboticaba", 15, "nivel 2"),
+    MAÇA ("kiwi", 36, "nivel 2"),
+    MELANCIA ("Melancia", 27, "nivel 2");
 
     private final String fruit;
     private final int value;
+    private final String nivel;
 
-    Fruits(String fruit, int value) {
+    private static final Random RANDOM = new Random();
+
+
+    Fruits(String fruit, int value, String nivel) {
         this.fruit = fruit;
         this.value = value;
-    }
-
-    public static Fruits obterFrutaAleatoria() {
-        Fruits[] frutas = values();
-        Random random = new Random();
-        int indiceAleatorio = random.nextInt(frutas.length);
-        return frutas[indiceAleatorio];
+        this.nivel = nivel;
     }
 
     public String getFruit() {
@@ -27,6 +30,23 @@ public enum Fruits {
 
     public int getValue() {
         return value;
+    }
+    public String getNiveis(){
+        return nivel;
+    }
+
+    public static Fruits obterFrutaAleatoria(String categoria) {
+        List<Fruits> frutasniveis = new ArrayList<>();
+        for (Fruits fruta : values()){
+            if (fruta.getNiveis().equals(categoria)) {
+                frutasniveis.add(fruta);
+            }
+        }
+         if (frutasniveis.isEmpty()){
+         throw new RuntimeException("nivel não encontrado: " + categoria);
+         }
+        int indiceAleatorio = RANDOM.nextInt(frutasniveis.size());
+        return frutasniveis.get(indiceAleatorio);
     }
 
     @Override
