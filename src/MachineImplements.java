646e30dd356ff334cfa47064a13c1c27d9fdaae6
raiani <raiani.guimaraes@zup.com.br>
2024-08-30 16:11:08 -0300

    adding machineimplements class

--- src/MachineImplements.java
+++ src/MachineImplements.java
@@ -5,35 +5,31 @@ public class MachineImplements implements Machine {
     private ArrayList<Fruits> listaDeFrutasAleatorias = new ArrayList<>();
     private int totalPontos = 0;
 
+    private String nivelPadrao = "nivel 1";
+
     @Override
-    public String getRandomFruits() {
+    public String getRandomFruits(String nivel) {
+        if (!nivel.equals("nivel 1") && !nivel.equals("nivel 2")) {
+            return "Nivel não existe. Use 'nivel 1' ou 'nivel 2'.";
+        }
         listaDeFrutasAleatorias.clear();
         totalPontos = 0;
 
-
         for (int index = 0; index < 3; index++) {
-            Fruits frutaAleatoria = Fruits.obterFrutaAleatoria();
+            Fruits frutaAleatoria = Fruits.obterFrutaAleatoria(nivel);
             listaDeFrutasAleatorias.add(frutaAleatoria);
             totalPontos += frutaAleatoria.getValue();
         }
-
-        String resultado = "";
+        StringBuilder resultado  = new StringBuilder();
         for (int index = 0; index < listaDeFrutasAleatorias.size(); index++) {
-            resultado += listaDeFrutasAleatorias.get(index);
+            resultado.append(listaDeFrutasAleatorias.get(index).getFruit());
             if (index < listaDeFrutasAleatorias.size() - 1) {
-                resultado += " | ";
+                resultado.append(" | ");
             }
+
         }
-
-
-        resultado += " \nTotal: " + totalPontos + " pontos.";
-
-        return resultado;
+        resultado.append(" \nTotal: ").append(totalPontos).append(" pontos.");
+        return resultado.toString();
     }
 
-
-    @Override
-    public int getTotalPoints() {
-        return totalPontos;
-    }
 }
